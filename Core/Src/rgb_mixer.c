c207a792355659746e787bd1a686db133e5849ff
christy antony <christyantony897@gmail.com>
2023-10-11 17:10:44 +0530

    CLOCK PART

--- Core/Src/rgb_mixer.c
+++ Core/Src/rgb_mixer.c
@@ -4,13 +4,29 @@
 #define CANVAS_WIDTH  200
 #define CANVAS_HEIGHT  150
 
+static lv_obj_t * meter;
+
+static void set_value(void * indic, int32_t v)
+{
+    lv_meter_set_indicator_end_value(meter, indic, v);
+}
+
 void rgb_mixer_create_ui(void)
 {
 	lv_obj_t* heading = lv_label_create(lv_scr_act());
 	lv_label_set_text(heading,"Analog Clock");
 	lv_obj_align(heading,LV_ALIGN_TOP_MID,0,30);
 
+	meter = lv_meter_create(lv_scr_act());
+	lv_obj_set_size(meter, 150, 150);
+	lv_obj_center(meter);
 
+	lv_meter_scale_t * scale_hour = lv_meter_add_scale(meter);
+	lv_meter_set_scale_ticks(meter, scale_hour, 12, 0, 0, lv_palette_main(LV_PALETTE_GREY));               /*12 ticks*/
+	lv_meter_set_scale_major_ticks(meter, scale_hour, 1, 2, 10, lv_color_black(), 10);    /*Every tick is major*/
+	lv_meter_set_scale_range(meter, scale_hour, 1, 12, 330, 300);       /*[1..12] values in an almost full circle*/
+
+	//canvas part
     /*lv_draw_rect_dsc_t rect_dsc;
     lv_draw_rect_dsc_init(&rect_dsc);
     rect_dsc.radius = 10;
@@ -25,23 +41,19 @@ void rgb_mixer_create_ui(void)
     rect_dsc.shadow_ofs_x = 5;
     rect_dsc.shadow_ofs_y = 5;
 
-
-
     static lv_color_t cbuf[LV_CANVAS_BUF_SIZE_TRUE_COLOR(CANVAS_WIDTH, CANVAS_HEIGHT)];
-
     lv_obj_t * canvas = lv_canvas_create(lv_scr_act());
     lv_canvas_set_buffer(canvas, cbuf, CANVAS_WIDTH, CANVAS_HEIGHT, LV_IMG_CF_TRUE_COLOR);
     lv_obj_align(canvas, LV_ALIGN_CENTER, 0, 0);
     lv_canvas_fill_bg(canvas, lv_color_black(), LV_OPA_COVER);
-
     lv_canvas_draw_rect(canvas, 70, 60, 100, 70, &rect_dsc);*/
 
-	static lv_style_t style;
-	lv_style_init(&style);
 
+	//circle part
+	/*static lv_style_t style;
+	lv_style_init(&style);
 	lv_style_set_arc_color(&style, lv_color_black());
 	lv_style_set_arc_width(&style, 4);
-
 
 	lv_obj_t * arc = lv_arc_create(lv_scr_act());
 	lv_obj_set_size(arc, 150, 150);
@@ -50,15 +62,7 @@ void rgb_mixer_create_ui(void)
 	lv_arc_set_value(arc, 0);
 	lv_obj_remove_style(arc, NULL, LV_PART_KNOB);
 	lv_obj_clear_flag(arc, LV_OBJ_FLAG_CLICKABLE);
-	lv_obj_add_style(arc, &style, 0);
-
-	 /*Add a scale first
-	lv_meter_set_scale_ticks(meter, 12, 2, 10,10, lv_color_black());
-	lv_meter_set_scale_major_ticks(meter, 8, 4, 15,10, lv_color_black(), 10);
-
-	lv_meter_indicator_t * indic;*/
-
-
+	lv_obj_add_style(arc, &style, 0);*/
 
 }
 
